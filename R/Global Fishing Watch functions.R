2ffa05a5532f6abb8ba72d3ab61a9f40a650ea55
Jack H Laverick <jacklaverick@ymail.com>
2020-05-20 22:57:01 +0100

    Add preliminary rough crop to function.

--- R/Global Fishing Watch functions.R
+++ R/Global Fishing Watch functions.R
@@ -9,15 +9,16 @@
 #'
 #' @param file The full name to a .csv file containing Global Fishing Watch data.
 #' @param area A Simple Feature object representing the areas to return fishing activity in.
-#' @return A dataframe of cells from a 0.01 degree grid which contain fishing activity,
-#' labelled by the FAO region they fall in.
-#' @family Global Fishing Watch functions
+#' @param box A 4 number vector of min and max longitude, min and max latitude in that order.
+#' @return A dataframe of cells from a 0.01 degree grid which contains fishing activity,
 #' @export
-get_cropped_fishing <- function (file, area) {
+get_cropped_fishing <- function (file, area, box) {
 
   Fish <- utils::read.csv(file, header = TRUE) %>%                           # Take first csv file as a test
     dplyr::mutate(Longitude = lon_bin/100, Latitude = lat_bin/100) %>%       # Move Coordinate columns, and divide by 100 because raw data misses decimal point
-    sf::st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>%
+    filter(between(Longitude, box[1], box[2]), 
+           between(Latitude, box[3], box[4])) %>%                            # Perform a rough crop before sf clipping 
+    sf::st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326, remove = TRUE) %>%
     sf::st_join(area) %>%
     tidyr::drop_na()
 }
